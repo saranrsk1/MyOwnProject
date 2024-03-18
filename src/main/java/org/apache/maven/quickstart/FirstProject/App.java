cc0a08ff36c986f9838fb179fd6e46003827dbfd
san@kum1 <saranyarsk1@gmail.com>
2024-03-18 11:43:15 +0530

    hello

---
+++ src/main/java/org/apache/maven/quickstart/FirstProject/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.quickstart.FirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/apache/maven/quickstart/FirstProject/MyFirstClass.java
@@ -0,0 +1,30 @@
+package org.apache.maven.quickstart.FirstProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class MyFirstClass {
+
+	public void launch()
+	{
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.navigate().to("https://www.gmail.com");
+		driver.navigate().refresh();
+		driver.navigate().back();
+		driver.navigate().forward();
+		//driver.manage().window().minimize();
+		//driver.close();
+		driver.quit();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFirstClass f = new MyFirstClass();
+		f.launch();
+
+	}
+
+}
